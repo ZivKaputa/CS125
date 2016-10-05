2952f97397e85702faedb13885d32f81134df7da
ZivKaputa <ziv.kaputa@gmail.com>
2016-10-05 18:11:33 -0500

    Add files via upload

--- RainGame.java
+++ RainGame.java
@@ -2,6 +2,7 @@ import java.awt.Graphics2D;
 
 /**
  * @author zkaputa2,jwrogge2
+ * @reviewed tjgrant2, exue3, denisbp2
  */
 public class RainGame {
 
@@ -19,45 +20,49 @@ public class RainGame {
 		String currentText = ""; //current text
 		String typed = ""; //the portion of the text already typed
 		boolean newLevel=true;
+		String temp = "";
+	
 		
 		
-		
-		//CHANGED
+		double lastCorrectTime = 0;
 		int level = 1;
-		long startTime =System.currentTimeMillis();
 		int entryPoint;
 		double dyingX=-100, dyingY=-100, dyingDX=0, dyingDY=0, dyingLevel=0, dyingOffsetX = 0,dyingOffsetY=0;
 		double deadTime = 0;
 		double newLevelTime = 0;
 		boolean dead = false;
+		int oldLevel = 1;
 		
-		//CHANGED
 		boolean started = false;
 		int successesUntilNextLevel=2;
 		int imageHeight = 50;
 		int offsetX = 0;
 		int offsetY = 0;
 		int speed = 0;
+		int asteroidIteration = 0;
 
 	
-		
-		/*Zen.drawImage("spaceBackground.jpg",0,0);
+		// Start Screen
+		Zen.drawImage("spaceBackground.jpg",0,0);
 		Zen.setFont("Courier-80");
 		Zen.setColor(256,256,256);
 		Zen.drawText("Numberoids",74,120);
 		Zen.setFont("Courier-15");
 		Zen.drawText("Destroy the asteroids by typing in the numbers",95,180);
+		Zen.drawText("Press Spacebar to advance the level",95,210);
 		Zen.drawText("PRESS ANY KEY TO BEGIN",130,250);
-		*/
+		
 		while (Zen.getEditText().isEmpty()){}
 		Zen.setEditText("");
 		
+		// Main loop
 		while (Zen.isRunning() && !dead) {
 
 
-			
+			Zen.setColor(0,0,0);
 			//Check if the user has finished typing the number
 			if (currentText.length() == 0) {
+				oldLevel = level;
 				
 				if (started){
 					dyingX=x;
@@ -75,7 +80,7 @@ public class RainGame {
 				entryPoint = (int) (Math.random() * 8);
 				
 
-				//Set the entry point at random
+				//Set the entry point of asteroid at random
 				switch (entryPoint){
 				case 0:
 					//Top Left
@@ -148,12 +153,11 @@ public class RainGame {
 				
 				
 				
-				
+				// generate random number on asteroid
 				currentText = "" + (int) (Math.random() * 999);
 				originalText = ""+currentText;
 				typed = "";
 				
-				startTime = System.currentTimeMillis();
 				if (started){
 					score += level;
 					successesUntilNextLevel--;
@@ -163,7 +167,7 @@ public class RainGame {
 			
 			
 			
-			
+			// advance the level by score
 			if (successesUntilNextLevel==0 && started){
 				successesUntilNextLevel=2;
 				level+=1;
@@ -172,11 +176,7 @@ public class RainGame {
 			}
 			
 			
-			
-			
 			started = true;
-			System.out.println(level);
-			
 			
 			//Find direction towards center
 			dx = (speed*(Zen.getZenWidth()/2.0-x)/Math.sqrt(Math.pow((Zen.getZenWidth()/2.0-x),2)+Math.pow((Zen.getZenHeight()/2.0-y),2)));
@@ -184,12 +184,24 @@ public class RainGame {
 			dyingDX = (speed*(Zen.getZenWidth()/2.0-dyingX)/Math.sqrt(Math.pow((Zen.getZenWidth()/2.0-dyingX),2)+Math.pow((Zen.getZenHeight()/2.0-dyingY),2)));
 			dyingDY = (speed*(Zen.getZenHeight()/2.0-dyingY)/Math.sqrt(Math.pow((Zen.getZenWidth()/2.0-dyingX),2)+Math.pow((Zen.getZenHeight()/2.0-dyingY),2)));
 			
+	
 			
-			//Draw Background and asteroid
+			
+			//Draw Background
 			Zen.drawImage("spaceBackground.jpg",0,0);
-			Zen.drawImage("asteroid.png",(int) (x+offsetX),(int) (y+offsetY),50,50);
+			
+			//Draw laser
 			Zen.setColor(255,0,0);
-			Zen.fillOval((int) x,(int) y,10,10);
+			if (System.currentTimeMillis() - lastCorrectTime < 150 && System.currentTimeMillis() - deadTime > 150){
+					Zen.drawLine(Zen.getZenWidth()/2, Zen.getZenHeight()/2, (int)x, (int)y);
+			}
+			else if (System.currentTimeMillis() - lastCorrectTime < 150 && System.currentTimeMillis() - deadTime < 150){
+				Zen.drawLine(Zen.getZenWidth()/2, Zen.getZenHeight()/2, (int)dyingX, (int)dyingY);
+			}
+			
+			
+			String asteroidFileName = "asteroid_01_no_moblur-"+asteroidIteration%8+"-"+(int)(asteroidIteration/8)%4+".png";
+			Zen.drawImage(asteroidFileName,(int) (x+offsetX),(int) (y+offsetY),50,50);
 			
 			//Detect collision
 			if (Math.pow((Zen.getZenWidth()/2-x),2)+Math.pow((Zen.getZenHeight()/2-y),2) <= 50*50){
@@ -201,13 +213,15 @@ public class RainGame {
 			
 			//Flash dead asteroid and score
 			if ((((int)(System.currentTimeMillis()-deadTime)/200)%2==1) && System.currentTimeMillis()-deadTime < 1000){
-				
-					Zen.drawImage("asteroid.png", (int) (dyingX+dyingOffsetX), (int) (dyingY+dyingOffsetY),50,50);
-					Zen.drawText("+2",300,200);
-		
+					
+					Zen.setFont("Courier-20");
+					Zen.setColor(240,240,240);
+					Zen.drawImage(asteroidFileName, (int) (dyingX+dyingOffsetX), (int) (dyingY+dyingOffsetY),50,50);
+					Zen.drawText("+"+oldLevel,300,355);
 			}
 			
 			if (System.currentTimeMillis()-newLevelTime < 1000 && newLevel){
+				Zen.setColor(240,240,240);
 				Zen.setFont("Courier-60");
 				Zen.drawText("Level "+level,175,80);
 				Zen.setFont("Courier-30");
@@ -220,21 +234,23 @@ public class RainGame {
 			}
 			
 			//Draw Center Circle
-			Zen.setColor(0, 256, 256);
-			Zen.fillOval(Zen.getZenWidth()/2-50, Zen.getZenHeight()/2-50, 100, 100);
+			Zen.drawImage("Earth.png",Zen.getZenWidth()/2-50, Zen.getZenHeight()/2-50, 100, 100);
 			Zen.setColor(255, 255, 255);
+			
 			
 			//Draw Number near asteroid
 			Zen.setFont("Courier-30");
+			Zen.setColor(255, 255, 255);
 			Zen.drawText(originalText, (int) x, (int) y);
 			Zen.setColor(255, 0, 0);
 			Zen.drawText(typed, (int) x, (int) y);
 			
 			//Draw Level and Score
+			Zen.setColor(255, 255, 255);
 			Zen.setFont("Courier-20");
-			Zen.drawText("Score",290,220);
+			Zen.drawText("Score",290,305);
 			Zen.setFont("Courier-30");
-			Zen.drawText(""+(int)score,Zen.getZenWidth()/2-(-6+12*(""+(int)score).length()),250);
+			Zen.drawText(""+(int)score,Zen.getZenWidth()/2-(-6+12*(""+(int)score).length()),335);
 			Zen.flipBuffer();
 			
 			//Move asteroid
@@ -258,12 +274,16 @@ public class RainGame {
 			// Reset the keyboard input to an empty string
 			// So next iteration we will only get the most recently pressed keys.
 			Zen.setEditText("");
-			
 			for(int i=0;i < user.length();i++) {
 				char c = user.charAt(i);
 				if(!currentText.isEmpty() && c == currentText.charAt(0)){
 					currentText = currentText.substring(1,currentText.length()); // all except first character
 					typed = typed + c;
+					lastCorrectTime = System.currentTimeMillis();
+				}
+				else if(!currentText.isEmpty() && c != currentText.charAt(0) && c != ' '){
+					currentText=originalText;
+					typed="";
 				}
 				if(c == ' '){
 					level ++;
@@ -273,6 +293,7 @@ public class RainGame {
 			}
 			
 			Zen.sleep(80);// sleep for 90 milliseconds
+			asteroidIteration++;
 
 		}
 		
@@ -285,10 +306,12 @@ public class RainGame {
 		Zen.drawText("Score: "+(int)score,95,180);
 		Zen.drawText("PRESS ANY KEY TO EXIT",130,250);
 		Zen.flipBuffer();
-		
+		Zen.setEditText("");
+		while(Zen.getEditText().isEmpty())
+			temp = Zen.getEditText();
+		System.exit(0);
 	}
 	
 
 
 }
-
